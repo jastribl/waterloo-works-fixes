580595079825c89e47551ceb771e4bbe74cc2a8b
Justin Stribling <justinastribling@gmail.com>
2017-09-28 16:13:08 -0400

    added background colours for app statues

--- content.js
+++ content.js
@@ -3,3 +3,12 @@ $(document).on('click', '.btn.btn-mini.btn-primary.dropdown-toggle', function ()
     $($(this).next()).find('a:last')[0].click();
 });
 
+var appStatues = $(".table.table-striped.table-bordered.table-hover.gridTable td:nth-child(2)");
+
+appStatues.filter(function() {
+    return $(this).text().includes("Selected for Interview");
+}).closest("tr").css("background-color", "rgba(0, 255, 0, 0.4)");
+
+appStatues.filter(function() {
+    return $(this).text().includes("Not Selected");
+}).closest("tr").css("background-color", "rgba(255, 0, 0, 0.4)");
