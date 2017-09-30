c83627b177e801748c84e978a75cba1e0ca295e0
Justin Stribling <justinastribling@gmail.com>
2017-09-29 22:16:33 -0400

    fixed to highlight properly, also shows red if interview selection complete and still applied status

--- content.js
+++ content.js
@@ -3,12 +3,12 @@ $(document).on('click', '.btn.btn-mini.btn-primary.dropdown-toggle', function ()
     $($(this).next()).find('a:last')[0].click();
 });
 
-var appStatues = $(".table.table-striped.table-bordered.table-hover.gridTable td:nth-child(2)");
+var appStatues = $("#postingsTableDiv td:nth-child(2)");
 
 appStatues.filter(function() {
     return $(this).text().includes("Selected for Interview");
-}).closest("tr").css("background-color", "rgba(0, 255, 0, 0.4)");
+}).parent().find("td").css("background-color", "rgba(0, 255, 0, 0.4)");
 
 appStatues.filter(function() {
-    return $(this).text().includes("Not Selected");
-}).closest("tr").css("background-color", "rgba(255, 0, 0, 0.4)");
+    return $(this).text().includes("Not Selected") || ($(this).text().includes("Applied") && $(this).parent().find("td:nth-child(8)").text().includes("Interview Selections Complete"));
+}).parent().find("td").css("background-color", "rgba(255, 0, 0, 0.4)");
