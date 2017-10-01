1ed69d476153ea6e7a24caed6a3e4e8242f40806
Justin Stribling <justinastribling@gmail.com>
2017-10-01 13:53:08 -0400

    added hacky timeout to redisplay highlighting on sort

--- content.js
+++ content.js
@@ -3,12 +3,25 @@ $(document).on('click', '.btn.btn-mini.btn-primary.dropdown-toggle', function ()
     $($(this).next()).find('a:last')[0].click();
 });
 
-var appStatues = $("#postingsTableDiv td:nth-child(2)");
+var highlightTimeout = null;
 
-appStatues.filter(function() {
-    return $(this).text().includes("Selected for Interview");
-}).parent().find("td").css("background-color", "rgba(0, 255, 0, 0.4)");
+var highlightJobStatus = function () {
+    var appStatues = $("#postingsTableDiv td:nth-child(2)");
 
-appStatues.filter(function() {
-    return $(this).text().includes("Not Selected") || ($(this).text().includes("Applied") && $(this).parent().find("td:nth-child(8)").text().includes("Interview Selections Complete"));
-}).parent().find("td").css("background-color", "rgba(255, 0, 0, 0.4)");
+    appStatues.filter(function() {
+        return $(this).text().includes("Selected for Interview");
+    }).parent().find("td").css("background-color", "rgba(0, 255, 0, 0.4)");
+
+    appStatues.filter(function() {
+        return $(this).text().includes("Not Selected") || ($(this).text().includes("Applied") && $(this).parent().find("td:nth-child(8)").text().includes("Interview Selections Complete"));
+    }).parent().find("td").css("background-color", "rgba(255, 0, 0, 0.4)");
+};
+
+$(document).on('click', 'a', function() {
+    if (highlightTimeout) {
+        clearTimeout(highlightTimeout);
+    }
+    highlightTimeout = setTimeout(highlightJobStatus, 1000);
+});
+
+highlightJobStatus();
