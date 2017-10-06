ce9b8652daaaebeaf1798037aa710882ed99c6c4
Justin Stribling <justinastribling@gmail.com>
2017-10-06 15:01:50 -0400

    updated logic for not selected students

--- content.js
+++ content.js
@@ -6,15 +6,19 @@ $(document).on('click', '.btn.btn-mini.btn-primary.dropdown-toggle', function ()
 var highlightTimeout = null;
 
 var highlightJobStatus = function () {
+    const RED_CSS = "rgba(255, 0, 0, 0.4)";
+    const GREEN_CSS = "rgba(0, 255, 0, 0.4)";
     var appStatues = $("#postingsTableDiv td:nth-child(2)");
 
     appStatues.filter(function() {
         return $(this).text().includes("Selected for Interview");
-    }).parent().find("td").css("background-color", "rgba(0, 255, 0, 0.4)");
+    }).parent().find("td").css("background-color", GREEN_CSS);
 
     appStatues.filter(function() {
-        return $(this).text().includes("Not Selected") || ($(this).text().includes("Applied") && $(this).parent().find("td:nth-child(8)").text().includes("Interview Selections Complete"));
-    }).parent().find("td").css("background-color", "rgba(255, 0, 0, 0.4)");
+        const internalStatus = $(this).parent().find("td:nth-child(8)").text();
+        const interviewsDone = internalStatus.includes("Interview Selections Complete") || internalStatus.includes("Interview Complete");
+        return $(this).text().includes("Not Selected") || ($(this).text().includes("Applied") && interviewsDone);
+    }).parent().find("td").css("background-color", RED_CSS);
 };
 
 $(document).on('click', 'a', function() {
